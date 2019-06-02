039bdfc7b2758dc51e828947cfb3b1b0f299687e
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2019-06-03 00:01:10 +0900

    add filters

--- chapter-2/app.js
+++ chapter-2/app.js
@@ -20,6 +20,14 @@ var vm = new Vue({
   el: '#app',
   data: {
     items: items
+  },
+  filters: {
+    numberWithDelimiter: function(value) {
+      if (!value) {
+        return '0';
+      }
+      return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+    }
   }
 });
 
